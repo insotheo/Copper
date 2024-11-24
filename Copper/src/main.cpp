34917f5f64c51a3e65d75ebea7082b035660b008
insotheo <maxim.insotheo@gmail.com>
2024-11-24 20:10:17 +0400

    Getting arguments from command line

--- Copper/src/main.cpp
+++ Copper/src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main(){
-    std::cout << "Hello, World!\n";
+int main(int argc, char* argv[]){
+    try{
+        if(argc != 2){
+            throw std::runtime_error("Invalid arguments amount!");
+        }
+
+        std::stringstream* inputstream = new std::stringstream();
+        std::ifstream* fs = new std::ifstream;
+        
+        fs->open(argv[1]);
+        if(!(fs->is_open())){
+            throw std::runtime_error("Error while trying to open the file!");
+        }
+
+        char c;
+        while(fs->get(c)){
+            *inputstream << c;
+        }
+        fs->close();
+        delete fs;
+
+        std::string input = inputstream->str();
+        delete inputstream;
+
+        std::cout << input << "\n";
+    }
+    catch(std::runtime_error& err){
+        std::cerr << err.what() << "\n";
+        return -1;
+    }
     return 0;
 }
